6a8f025b10c746b40e89b07251a934c4dff427b4
Zineb <zinebelkanouni4@gmail.com>
2026-03-05 17:49:43 +0100

    Premier commit projet Spring Boot

---
+++ src/main/java/com/Zineb/gestionfacturation/GestionfacturationApplication.java
@@ -0,0 +1,13 @@
+package com.Zineb.gestionfacturation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionfacturationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestionfacturationApplication.class, args);
+	}
+
+}
